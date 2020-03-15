f2138bda986bbe412b73ddc4c65344bf82ed8e61
Daniel Chicot <dan.chicot@gmail.com>
2020-03-15 18:58:18 +0000

    Renamed module.

---
+++ chicken_time.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+""" Calculate the time a chicken needs to spend in the oven. """
+
+import argparse
+
+def main():
+    """ Main entry point. """
+    parser = argparse.ArgumentParser(description='Calculate chicken roast time.')
+    parser.add_argument('weight',
+                        metavar='kg',
+                        type=float,
+                        help="Weight of chicken in kilograms")
+    args = parser.parse_args()
+    weight_of_chicken = float(args.weight)
+    time_in_oven = 30 + 20 * weight_of_chicken / .5
+    print(time_in_oven)
+
+
+if __name__ == "__main__":
+    main()
